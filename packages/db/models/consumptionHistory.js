c041eee74d27297a0247585a18bf042642effc43
Emanuel Ureña <eurena@soin.co.cr>
2020-02-28 15:11:00 -0600

    feat(api): logger launch update

---
+++ packages/db/models/consumptionHistory.js
@@ -0,0 +1,52 @@
+module.exports = (sequelize, DataTypes) => {
+    const ConsumptionHistory = sequelize.define(
+      'ConsumptionHistory',
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: DataTypes.INTEGER,
+        },
+        lastConsumption: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            references: {
+                model: 'Users',
+                key: 'id',
+            },
+            onUpdate: 'CASCADE',
+            onDelete: 'SET NULL',
+        },    
+        beerId: {
+          type: DataTypes.INTEGER,
+          allowNull: true,
+          references: {
+              model: 'Beers',
+              key: 'id',
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'SET NULL',
+      },    
+      },
+      {
+        tableName: 'ConsumptionHistorys',
+      }
+    )
+
+    ConsumptionHistory.associate = models => {
+      ConsumptionHistory.belongsTo(models.User, {
+        foreignKey: 'userId',
+      })
+      ConsumptionHistory.belongsTo(models.Beer, {
+        foreignKey: 'beerId',
+      })
+    }
+  
+    return ConsumptionHistory
+  }
+  
